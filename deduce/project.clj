2348313ce28785adc073c87d4c32c2f449c6941b
aeskilso <a456e193@ku.edu>
2015-03-20 17:48:02 -0500

    progress\!

--- deduce/project.clj
+++ deduce/project.clj
@@ -6,6 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.priority-map "0.0.5"]
-                 [org.clojure/data.int-map "0.1.0"]]
+                 [org.clojure/tools.trace "0.7.8"]]
   :source-paths ["src"]
   :main deduce.core)
--- deduce/src/deduce/resolve.clj
+++ deduce/src/deduce/resolve.clj
@@ -6,7 +6,9 @@
 
 (defn variable? 
   [x]
-  (= (first (name x)) \?))
+  (if (or (nil? x) (list? x))
+    false
+    (= (first (name x)) \?)))
 
 (defn negation? 
   [x]
@@ -14,7 +16,7 @@
 
 (defn negated-term? 
   [x]
-  (negation? (first (name x))))
+  (negation? (first (name (first x)))))
 
 (defn value 
   [x th]
@@ -26,7 +28,7 @@
 
 (defn get-binding 
   [k th]
-  '(k (value k th)))
+  '(k (get k th)))
 
 (defn binding-var
   [bind]
@@ -46,12 +48,26 @@
 
 (defn reuse-cons
   [x y x-y]
-  (if (and (= x (first x-y)) (= y (rest x-y))) x-y [x y]))
+  (if (and (= x (first x-y)) (= y (rest x-y))) x-y '(x y)))
 
 (defn pair?
   [x]
-  (= (count x) 2))
+  (if (list? x) 
+    (= (count x) 2)
+    false))
 
+(defn substitute-bindings
+  [bindings exp]
+  (cond
+    (= bindings false) false
+    (empty? bindings) exp
+    :else (for [sym exp]
+            (cond
+              (list? sym) (substitute-bindings bindings sym)
+              (and (variable? sym) (contains? bindings sym)) (get bindings sym)
+              :else sym))))
+
+(comment
 (defn substitute-bindings
   [bindings x]
   (cond
@@ -64,7 +80,7 @@
     :else (reuse-cons (substitute-bindings bindings (first x))
                       (substitute-bindings bindings (rest x))
                       x)))
-
+)
 (defn occur?
   [v t th]
   (cond
@@ -74,22 +90,29 @@
     (not (pair? t)) false
     :else (some true? (map (fn [y] (occur? v y th)) (rest t)))))
 
+(defn merge-quit
+  [m r]
+  (if r
+    (merge m r)
+    false))
+
 (defn unify
-  [p q th]
-  (= th false) false
-  (and (variable? p) (isbound? p th)) (unify (value p th) q th)
-  (and (variable? q) (isbound? q th)) (unify p (value q th) th)
-  (= p q) th
-  (variable? p) (if (occur? p q th)
-                  false
-                  (assoc th p q))
-  (variable? q) (if (occur? q p th)
-                  false
-                  (assoc th q p))
-  (not (and (pair? p) (pair? q))) false
-  (not= (first p) (first q)) false
-  (not= (count p) (count q)) false
-  :else (reduce unify th (rest p) (rest q)))
+  [th p q]
+  (cond
+    (= th false) false
+    (and (variable? p) (isbound? p th)) (unify th (value p th) q)
+    (and (variable? q) (isbound? q th)) (unify th p (value q th))
+    (= p q) th
+    (variable? p) (if (occur? p q th)
+                    false
+                    (assoc th p q))
+    (variable? q) (if (occur? q p th)
+                    false
+                    (assoc th q p))
+    (and (symbol? p) (symbol? q) (not= p q)) false
+    (not= (first p) (first q)) false
+    (not= (count p) (count q)) false
+    :else (reduce merge-quit {} (map (fn [pi qi] (unify th pi qi)) (rest p) (rest q)))))
 
 (defn instantiate
   [x e]
@@ -129,20 +152,38 @@
 
 (defn unifier
   [p q]
-  (instantiate p (unify p (rename-variables q) {})))
+  (instantiate p (unify {} p (rename-variables q))))
 
 (defn resolver
   [lc kd]
   (if (negated-term? (first lc))
     (let [renamed-lc (rename-clause lc)
-          unifiable (unify (first (rest (first renamed-lc))) (first kd) {})]
+          unifiable (unify {} (first (rest (first renamed-lc))) (first kd))]
       (if unifiable
-        (rename-clause (instantiate-clause (concat (rest (renamed-lc)) (rest kd)) unifiable))
+        (rename-clause (instantiate-clause (concat (rest renamed-lc) (rest kd)) unifiable))
         false))
     false))
 
+(def definite-horn-clauses
+  '(((Criminal ?x) (¬(American ?x)) (¬(Weapon ?y)) (¬(Sells ?x ?y ?z)) (¬(Hostile ?z)))
+    ((Enemy Nono America))
+    ((Owns Nono M1 ))
+    ((Missile M1 ))
+    ((Sells West ?x Nono) (¬(Missile ?x)) (¬(Owns Nono ?x)))
+    ((American West))
+    ((Weapon ?x) (¬(Missile ?x)))
+    ((Hostile ?x) (¬(Hostile (Mother-of ?x) )))
+    ((Hostile ?x) (¬(Enemy ?x America)))
+    ((Hostile ?x) (¬(Hostile (Father-of ?x) )))))
+
+(defn find-moves
+  [state clauses]
+  (filter (fn [r] (not= r false)) 
+          (map (fn [c] (resolver state c)) 
+               clauses)))
+
 (defn res-move
-  [s clauses]
+  [s]
   (filter (fn [rhs] (let [resolved (resolve s rhs)]
                       (if resolved 
                         (list resolved (first rhs) 1)
