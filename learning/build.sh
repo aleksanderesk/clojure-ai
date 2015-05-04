b781385a151a0025d2c3d9d9c431b2fbe2e2c542
aleksanderesk <aleksanderesk@gmail.com>
2015-05-04 12:29:19 -0500

    fixed build mistakes

--- learning/build.sh
+++ learning/build.sh
@@ -3,4 +3,4 @@ wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
 ./lein deps
 ./lein uberjar
-java -jar target/astar-0.1.0-SNAPSHOT-standalone.jar
+java -jar target/learning-0.1.0-SNAPSHOT-standalone.jar
--- learning/project.clj
+++ learning/project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [incanter "1.5.6"]
-                 [net.mikera/vectorz-clj "0.29.0"]])
+                 [net.mikera/vectorz-clj "0.29.0"]]
+  :main learning.core)
